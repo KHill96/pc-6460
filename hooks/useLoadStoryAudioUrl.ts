2cd9f28735889e2a790a2c7421d6b4bb3076f7a6
Kenneth Hill <khill0996@gmail.com>
2024-03-31 21:55:07 -0400

    Removed file

--- hooks/useLoadStoryAudioUrl.ts
+++
@@ -1,9 +0,0 @@
-import { Story } from "@/types";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
-
-const useLoadStoryAudioUrl = (story: Story) => {
-    if (!story) return ''
-    return story.audio_url
-}
- 
-export default useLoadStoryAudioUrl;
