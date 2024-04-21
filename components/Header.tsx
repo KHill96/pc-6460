97073629208e7a133daf9b73c6706c2e8e6c1f5b
Kenneth Hill <khill0996@gmail.com>
2024-04-21 09:40:01 -0400

    Better suited for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -32,8 +32,12 @@ const Header:React.FC<HeaderProps> = ({children, className}) => {
     return ( 
         <div className={twMerge(`h-fit p-6 bg-gradient-to-b from-sky-800`, className)}>
             <div className="w-full mb-4 flex items-center justify-between ">
-                <div className="max-lg:hidden md:flex gap-x-2 items-center">
-                      
+                <div className="max-lg:hidden md:flex gap-x-10 items-center scale-110 px-4">
+                <a href="/">
+                        <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+                        <ImLibrary className="text-black" size={20}/>
+                        </button>
+                    </a>  
                     <form className='flex flex-row gap-x-2' onSubmit={(e) => {
                         e.preventDefault()
                         router.push(inputValue)
@@ -44,7 +48,7 @@ const Header:React.FC<HeaderProps> = ({children, className}) => {
                     </button>
                     </form>
                 </div>
-                <div className="flex md:hidden gap-x-5 pb-5 items-center justify-center ">
+                <div className="flex md:hidden gap-x-5 pb-5 items-center justify-center">
                     <a href="/">
                         <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
                         <ImLibrary className="text-black" size={20}/>
@@ -59,7 +63,34 @@ const Header:React.FC<HeaderProps> = ({children, className}) => {
                     </form>
                 </div>
                 <div className="flex justify-between items-center gap-x-4">
+                {/* {user ? (
+                            <div className="flex gap-x-4 items-center">
+                                <Button onClick={handleLogout} className="bg-white px-6 py-2 text-black">Logout</Button>
+                                <Button className="bg-white text-black rounded-full" onClick={() => router.push('/account')}>
+                                    <FaUserAstronaut />
+                                </Button>
+                            </div>
+                        ) : (
+                            <>
+                            <div className="flex md:hidden gap-x-5 pb-5 items-center justify-center text-sm">
+                                <div>
+                                <Button onClick={onOpen} className="bg-transparent font-medium text-neutral-300">Sign Up</Button>
+                                </div>
+                                <div>
+                                    <Button onClick={onOpen} className="px-4 py-1 text-black">Log In</Button>
+                                </div>
+                            </div>
+                            <div className="max-lg:hidden flex gap-x-5">
+                                    <div>
+                                        <Button onClick={onOpen} className="bg-transparent font-medium text-neutral-300">Sign Up</Button>
+                                    </div>
+                                    <div>
+                                        <Button onClick={onOpen} className="px-6 py-2 text-black">Log In</Button>
+                                    </div>
+                                </div></>
+                        )
                     
+                    } */}
                 </div>
             </div>
             {children}
--- components/StoryContent.tsx
+++ components/StoryContent.tsx
@@ -17,7 +17,7 @@ const StoryContent:React.FC<StoryContentPropts> = ({details}) => {
        <div className="text-2xl">
             <p className="text-center">{details.title}</p>
        </div>
-       <div className="pt-10 flex-column">
+       <div className="pt-10 flex-column w-auto">
                 <div className="flex">
                     <Image
                         src={details.cover_url}
@@ -26,18 +26,18 @@ const StoryContent:React.FC<StoryContentPropts> = ({details}) => {
                         height={100}
                         className="px-5 object-fit max-h-[400px] max-w-[500px]" />
                     <div>
-                        <div className="md:text-md sm:text-lg">
-                            <span className="text-xs m-5">
+                        <div className="">
+                            <span className="text-xl max-lg:text-xs">
                                 {details.description}
                             </span>
                         </div>
-
                     </div>
                 </div>
                 <div className="py-5"></div>
                 <div className="flex flex-row gap-x-5">
                     <Link href={details.epub_url} className="my-auto py-2 rounded-lg px-2 bg-neutral-400/10 scale-100 hover:bg-neutral-400/20 hover:scale-110 transition"> <span> Download epub</span>  </Link>
                     <Link href={details.pdf_url} className="text-lg my-auto py-2 rounded-lg px-2 bg-neutral-400/10 scale-100 hover:bg-neutral-400/20 hover:scale-110 transition"> <span> Download pdf</span>  </Link>
+                    <Link href={"/render/"+details.id} className="text-lg my-auto py-2 rounded-lg px-2 bg-neutral-400/10 scale-100 hover:bg-neutral-400/20 hover:scale-110 transition"><span>Read Now</span></Link>
                 </div>
             </div></>
     );
