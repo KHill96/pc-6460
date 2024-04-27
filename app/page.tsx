cf353d95b554588d981bff5af30d53c72b6d9014
Kenneth Hill <khill0996@gmail.com>
2024-04-27 08:57:53 -0400

    Changed title

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 import Header from "@/components/Header";
-import ListItem from "@/components/ListItem";
 import PageContent from "@/components/PageContent";
 import getStories from "@/actions/getStories";
-
-import Link from "next/link";
-import Image from "next/image";
 
 export const revalidate = 0;
 
@@ -14,18 +10,18 @@ export default async function Home() {
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
         <div className="mb-2">
-          <h1 className="text-white text-3xl font-semibold">
-            Welcome!
+          <h1 className="text-white text-center text-3xl font-semibold">
+            Public Ebooks
           </h1>
           
         </div>
       </Header>
       <div className="mt-2 mb-7 px-6">
-        <div className="flex justify-between items-center font-semibold">
+        {/* <div className="flex justify-between items-center font-semibold">
           <h1>
             Newest Additions
           </h1>
-        </div>
+        </div> */}
       <div>          
           <PageContent stories={stories}/>
       </div>
